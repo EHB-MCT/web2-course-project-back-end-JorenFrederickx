fc8d785f863f1a62b93f42e83f4f5067fdb9d4f7
JorenFrederickx <joren.frederickx@student.ehb.be>
2026-01-02 13:13:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
-console.log("BOOT: starting server...");
 const express = require("express");
-const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Listening on", port));
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("API is running");
 });
 
-// POST route for login
-app.post("/login", (req, res) => {
-	// const { username, password } = req.body;
-	const username = req.body.username;
-	const password = req.body.password;
-	console.log("username:", username);
-	console.log("password:", password);
-
-	res.send("Login endpoint");
-});
-console.log("BOOT: about to listen on", process.env.PORT);
-// Start the API
-app.listen(port, () => {
-	console.log(`API server running at http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`API server running on port ${PORT}`);
 });
