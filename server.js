52e369fe8df8ee67f9a2f4594cc976901816f090
JorenFrederickx <joren.frederickx@student.ehb.be>
2026-01-02 01:44:20 +0100

    Update om te hosten

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
 // POST route for login
 app.post("/login", (req, res) => {
+	// const { username, password } = req.body;
+	const username = req.body.username;
+	const password = req.body.password;
+	console.log("username:", username);
+	console.log("password:", password);
+
 	res.send("Login endpoint");
 });
 
